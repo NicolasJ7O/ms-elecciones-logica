30893bb33968727989b0ba95591aad25b5d02944
Nicolas <nicolas.1701812174@ucaldas.edu.co>
2022-08-29 19:51:44 -0500

    models finished and repositorys created

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,3 +6,6 @@ export * from './votante.model';
 export * from './bloque-votante.model';
 export * from './bloque-cargo.model';
 export * from './cargo.model';
+export * from './plancha.model';
+export * from './voto.model';
+export * from './candidato.model';
